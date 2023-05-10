a0c5a8a98ca3ee35e096b58d69c88f5f21b4ff31
kathiriya18 <131762299+kathiriya18@users.noreply.github.com>
2023-05-10 15:49:34 +0530

    Add files via upload

---
+++ KKH20.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+     int i=1,n;
+     clrscr();
+     printf("enter number:-");
+     scanf("%d",&n);
+     while (i<=n)
+     {
+       printf("%d\n",i);
+       i++;
+     }
+     getch():
+
+}
